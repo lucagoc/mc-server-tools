4184bf20a137e45485408e47732c5b095a264787
lucagoc <77584602+lucagoc@users.noreply.github.com>
2024-01-02 20:49:09 +0100

    Mise en place d'un backup_history

--- bash/do-server-backup.sh
+++ bash/do-server-backup.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-# Ouverture du répertoire
-cd /home/ubuntu/
+date_formatted=$(date +"%Y-%m-%d %H:%M:%S")
+echo "[$date_formatted] Démarrage d'une backup sur $a_server_name... " >> backup_history
+
+echo "   _____           _       _     _                _                ";
+echo "  / ____|         (_)     | |   | |              | |               ";
+echo " | (___   ___ _ __ _ _ __ | |_  | |__   __ _  ___| | ___   _ _ __  ";
+echo "  \___ \ / __| '__| | '_ \| __| | '_ \ / _\` |/ __| |/ / | | | '_ \ ";
+echo "  ____) | (__| |  | | |_) | |_  | |_) | (_| | (__|   <| |_| | |_) |";
+echo " |_____/ \___|_|  |_| .__/ \__| |_.__/ \__,_|\___|_|\_\\__,_| .__/ ";
+echo "                    | |                                     | |    ";
+echo "                    |_|                                     |_|    ";
+echo "Note: La dernière backup effectuée avec succès a été faite sur $b_server_name à "
+echo "Démarrage de la backups sur $a_server_name dans 10 secondes..."
 
 # Fermeture du proxy et des serveurs
+
+echo "(1/3) Fermeture des services... "
 sudo systemctl stop velocity
 sudo systemctl stop mc-lobby
 sudo systemctl stop mc-survie
 
 # Synchronisation
+echo "(2/3) Synchronisation avec le serveur $server_name... "
 rclone sync /opt/mc-server mega: --delete-excluded -v
 
 # Note :
@@ -23,7 +37,7 @@ if [ $? -eq 0 ]; then
 else
 	echo "ERREUR: Impossible d'effectuer la sauvegarde correctement, relancement des services dans 10s... "
 	sleep 10
-	sudo systemctl stop velocity
+	sudo systemctl start velocity
 	sudo systemctl start mc-lobby
 	sudo systemctl start mc-survie
 fi
